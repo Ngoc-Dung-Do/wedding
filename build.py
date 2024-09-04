988898e1875880145b09a284f75fdb5e0dbad805
Hùng Nguyễn <ndgnuh@protonmail.com>
2024-09-05 00:24:04 +0700

    [build] test wedding invitation

--- build.py
+++ build.py
@@ -1,13 +1,13 @@
+import json
 import os
+import random
 import shutil
-from argparse import ArgumentParser
 from os import path
 
 import yaml
-from css_html_js_minify import (process_single_css_file,
-                                process_single_html_file,
-                                process_single_js_file)
 from jinja2 import Environment, FileSystemLoader
+from PIL import Image
+from slugify import slugify
 
 
 def get_files(rootdir: str):
@@ -44,19 +44,18 @@ def build(
     """Render jinja template"""
     env = Environment(loader=FileSystemLoader("."))
     template = env.get_template("template.html")
-    sprites = sorted(get_files("images/sprites/"))
-    album_images = sorted(get_files("images/album/"))
-    carousel_images = sorted(get_files("images/carousel/"))
-    rendered = template.render(
-        album_images=album_images,
-        carousel_images=carousel_images,
-        sprites=sprites,
+    invitation_template = env.get_template("images/invitation/card-template.svg")
+    ctx = dict(
+        sprites=sorted(get_files("images/sprites/")),
+        album_images=sorted(get_files("images/album/")),
+        carousel_images=sorted(get_files("images/carousel/")),
         footer_image=data["footer-image"],
         bride_image=data["bride-image"],
         groom_image=data["groom-image"],
         donations=data["donations"],
         events=data["events"],
     )
+    rendered = template.render(**ctx)
 
     """install files"""
     install_files = [
@@ -81,6 +80,9 @@ def build(
     for file in install_files:
         if path.isfile(file):
             continue
+        dest = path.join(output_dir, file)
+        print(f"making directory {dest}")
+        os.makedirs(dest, exist_ok=True)
         for root, dirs, _ in os.walk(file):
             for dir in dirs:
                 dest = path.join(output_dir, root, dir)
@@ -102,6 +104,46 @@ def build(
     with open(out_index, "w", encoding="utf-8") as f:
         f.write(rendered)
 
+    """Invitation list"""
+    inv_dir = os.path.join(output_dir, "invitation")
+    try:
+        shutil.rmtree(inv_dir)
+    except Exception:
+        pass
+    os.makedirs(inv_dir, exist_ok=True)
+    with open("guest-list.json", encoding="utf-8") as f:
+        guests = json.load(f)
+
+    inv_paths = []
+    for _, guest_group in guests.items():
+        salt = f"{random.random():0.6f}".split(".")[1]
+        card_url = guest_group["card"]
+        for guest_name in guest_group["guests"]:
+            # Render
+            rendered = template.render(
+                **ctx,
+                guest_name=guest_name,
+                invite_card_url=card_url,
+            )
+            rendered_card = invitation_template.render(guest_name=guest_name)
+
+            # Write rendered outputs
+            out_path = path.join(inv_dir, slugify(guest_name) + "-" + salt)
+            os.makedirs(out_path, exist_ok=True)
+            with open(path.join(out_path, "index.html"), "w") as f:
+                f.write(rendered)
+            with open(path.join(out_path, "invitation.svg"), "w") as f:
+                f.write(rendered_card)
+
+            # Save invitation slugs
+            url = "https://ngoc-dung-do.github.io"
+            inv_paths.append(f"{guest_name}: {url}/{out_path}")
+
+    # Save invitation links
+    inv_links_path = path.join(output_dir, "INVITATION_LINKS.txt")
+    with open(inv_links_path, "w", encoding="utf-8") as f:
+        f.write("\n".join(inv_paths))
+    print("Invitation link: " + inv_links_path)
     print("DONE!")
 
 
